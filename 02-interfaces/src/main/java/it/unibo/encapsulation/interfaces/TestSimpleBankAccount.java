91bedc08e537348470ce8a0112b964647bf00422
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-24 11:22:24 +0200

    Edited objects initialization in TestSimpleBankAccount

--- 02-interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestSimpleBankAccount.java
+++ 02-interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestSimpleBankAccount.java
@@ -9,15 +9,17 @@ public class TestSimpleBankAccount {
     }
     public static void main(final String[] args) {
         // 1) Creare l' AccountHolder relativo a Andrea Rossi con id 1
-        AccountHolder andrea = new AccountHolder("Andrea", "Rossi", 1);
+        AccountHolder user1 = new AccountHolder("Andrea", "Rossi", 1);
         // 2) Creare l' AccountHolder relativo a Alex Bianchi con id 2
-        AccountHolder alex = new AccountHolder("Alex", "Bianchi", 2);
+        AccountHolder user2 = new AccountHolder("Alex", "Bianchi", 2);
         // 3) Creare i due SimpleBankAccount corrispondenti
-        SimpleBankAccount acc1 = new SimpleBankAccount(1, 5000);
-        SimpleBankAccount acc2 = new SimpleBankAccount(2, 10000);
+        SimpleBankAccount acc1 = new SimpleBankAccount(user1.getUserID(), 5000);
+        SimpleBankAccount acc2 = new SimpleBankAccount(user2.getUserID(), 10000);
+
         // 4) Effettuare una serie di depositi e prelievi
         
-
+        acc1.deposit(1,200);
+        acc1.withdrawFromATM(1, 1500);
 
         /*
          * 5) Stampare a video l'ammontare dei due conti e verificare la
