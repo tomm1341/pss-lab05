c9cf12f1e56d68b15af57adeb07f311b07c4de2b
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-26 11:04:41 +0200

    Modified gradle file

--- 02-interfaces/build.gradle.kts
+++ 02-interfaces/build.gradle.kts
@@ -3,4 +3,5 @@ application }
 
 application {
     mainClass.set("it.unibo.encapsulation.interfaces.TestSimpleBankAccount")
+    mainClass.set("it.unibo.encapsulation.interfaces.TestBanckAccount")
 }
