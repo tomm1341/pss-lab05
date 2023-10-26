0de34eca8a61a06b48969fee94314d0bfa827bf9
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-26 17:56:00 +0200

    Completed Testing.java until point 4

--- 03-composition/src/main/java/it/unibo/composition/Testing.java
+++ 03-composition/src/main/java/it/unibo/composition/Testing.java
@@ -4,6 +4,9 @@ public class Testing {
 
     public static void main(final String[] args) {
 
+        final String[] courses1 = {"Latino", "Greco"};
+        final String[] courses2 = {"Informatica", "Analisi"};
+
         // 1)Creare 3 studenti a piacere
 
         Student student1 = new Student(1, "Giovanni", "Bianchi", "1234", 2022);
@@ -12,12 +15,22 @@ public class Testing {
 
         // 2)Creare 2 docenti a piacere
 
+        Professor professor1 = new Professor(1, "Luigi", "Grigi", "123abc", courses1);
+        Professor professor2 = new Professor(2, "Gennaro", "Esposito", "abc123", courses2);
+
         // 3) Creare due aulee di esame, una con 100 posti una con 80 posti
+
+        ExamRoom room1 = new ExamRoom(100, "Aula 2.1", true, true);
+        ExamRoom room2 = new ExamRoom(80, "Aula 3.3", true, true);
 
         // 4) Creare due esami, uno con nMaxStudents=10, l'altro con
         // nMaxStudents=2
 
+        Exam exam1 = new Exam(1, 10, 0, "Latino", professor1, room2, null)
+
         // 5) Iscrivere tutti e 3 gli studenti agli esami
+
+        
 
         // 6) Stampare in stdout la rapresentazione in stringa dei due esami
     }
